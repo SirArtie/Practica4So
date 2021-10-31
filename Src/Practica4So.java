7c50b42e24c026456a1f4f5150083516998015e3
SirArtie <60563334+SirArtie@users.noreply.github.com>
2021-10-31 09:00:14 -0600

    Cambios
    Se actualizo la entrega final para trabajar a partir de ella

--- Src/Practica4So.java
+++ Src/Practica4So.java
@@ -4,77 +4,34 @@
  * @author Artie
  * @author: Tere
  */
-public class Practica4So {
-    public static void main(String[] args) {
-        String m = "1.-Crear Proceso Nuevo\n2.-Ver estado actual del Sistema\n3.-Imprimir Cola de Procesos\n4.-Ver proceso actual\n5.-Ejecutar proceso actual\n6.-Pasar al proceso siguiente\n7.-Matar proceso actual\n8.-Salir del programa\n";
-        int opcion;
-        //Se inicializa el administrador del manejo de procesos
-        manejoProcesos controlador = new manejoProcesos();
-        //Se inicializa el administrador de la memoria
-        gestionMemoria memoria = new gestionMemoria();
-        //Se inicializa la memoria, el segmento vacio
-        memoria.memoriaVacia();
-        do{
-            //Funcion que permite pedir un meno continuamente
-            opcion = Utilidades.rMenu(m,8);
-            switch(opcion){
-              case 1:
-                    System.out.println("********** Crear Proceso *******************");
-                    String mensajeProceso = "Ingresa el nombre del proceso";
-                    /*Nota de Axel: El parametro de funcion, es basicamente una Funcion
-                    que devuelve una cadena, esto es simplemente para simplificar el proceso*/
-                    controlador.crearProceso(Utilidades.giveAString(mensajeProceso),memoria);
-                    break;
-              case 2:
-                    System.out.println("");
-                    break;
-              case 3:
-                    System.out.println("************** Imprimir cola de procesos *****************");
-                    controlador.imprimirInformacionPreparados();
-                    break;
-              case 8:
-                    System.out.println("Adios, vuelva pronto");
-                    break;
-              default:
-                    System.out.println("La opción que ha insertado no es valida");
-                    break;
-            }
+ /*
 
-            //memoria.buscarSegmentoVacio();
-            //Uso esta funcion para ver el estado de la memoria
-            memoria.imprimirListaMemoria();
-        }while(opcion!=8);
-
-    }
-
-}
+//----------------------------------------------------------------------------------------------------------------------------------
 
 
-
-----------------------------------------------------------------------------------------------------------------------------------
-    
-  /*  
     public class Practica4SO {
-    
+
     /**
      * @param args the command line arguments
      */
+  public class Practica4So{
+
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         System.out.println("\n****************************************************************");
         System.out.println("********************** GESTIÓN DE MEMORIA **********************");
         System.out.println("****************************************************************\n");
-        
+
         String m = "MENÚ DE OPCIONES:\n1.-Crear Proceso Nuevo\n2.-Ver estado actual del Sistema\n3.-Imprimir Cola de Procesos\n4.-Ver proceso actual\n5.-Ejecutar proceso actual\n6.-Pasar al proceso siguiente\n7.-Matar proceso actual\n8.-Salir del programa\n";
         int opcion;
-        
+
         //Se inicializa el administrador del manejo de procesos
         ManejoProcesos controlador = new ManejoProcesos();
-        
+
         //Se inicializa el administrador de la memoria
         gestionMemoria memoria = new gestionMemoria();
-        
+
         memoria.memoriaVacia();
         do{
             //Funcion que permite pedir un meno continuamente
@@ -117,7 +74,7 @@ public class Practica4So {
                 case 6:
                     System.out.println("\n----------------------------------------------------------------\n");
                     System.out.println("******************** Pasar al proceso siguiente ********************\n");
-                    controlador.PasarProcesoSiguiente();                    
+                    controlador.PasarProcesoSiguiente();
                     System.out.println("\n----------------------------------------------------------------\n");
                     break;
                 case 7:
@@ -153,6 +110,5 @@ public class Practica4So {
             //memoria.imprimirListaMemoria();
         }while(opcion!=8);
     }
-    
+
 }
-*/
--- Src/gestionMemoria.java
+++ Src/gestionMemoria.java
@@ -1,110 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
-public class gestionMemoria {
-  /*En esta implementacion usa la idea de cola de memoria, por lo cual
-  cada segmento sera un pedazo de memoria en el cual hay un proceso y
-  esta delimitado por un registro base y limite, en donde demarca
-  las localidades abarcadas por el segmento, independientemente
-  de si este vacio o tenga algun segmento*/
 
-  List<Segmento> memoriaPrincipal = new ArrayList<Segmento>();
-  //Aqui se puede observar el objeto segmento, el cual sera la unidad basica
-  //de nuestra estructura memoriaPrincipal, la cual es una lista
-  public class Segmento{
-    Proceso P;
-    int base;
-    int limite;
-  }
-
-  //Esta funcion inicializa la lista con el segmento 0 a 2048 que representa la memoria
-  void memoriaVacia(){
-    Proceso vacio = new Proceso("Vacio",0,0,0);
-    Segmento primerSegmento = new Segmento();
-    primerSegmento.P = vacio;
-    primerSegmento.base = 0;
-    primerSegmento.limite = 2048;
-    memoriaPrincipal.add(primerSegmento);
-  }
-
-  /*Para insertar necesitamos buscar espacio en la lista de memoria, por lo cual primero
-  debemos localizar un espacio en donde colocar el proceso y posteriormente insertarlo,
-  aunque esta funcion en concreto solo se encarga de la parte de la localizacion*/
-  /*Agregado valor bool, para poder verificar si un proceso debe terminar de ser creado o no*/
-  boolean buscarSegmentoVacio(Proceso p){
-    int espacio;
-    for( int i = 0; i<memoriaPrincipal.size(); i++){
-
-      espacio = memoriaPrincipal.get(i).limite - memoriaPrincipal.get(i).base;
-      //Con el siguiente if, verificamos que el segmento este vacio y ademas
-      //pueda caber nuestro proceso, en terminos de algoritmo seria un PRIMER ENCAJE
-      if(memoriaPrincipal.get(i).P.nombre == "Vacio" && p.r_localidad<=espacio){
-        //Una vez encontrado un segmento, podemos insertar
-        insertarSegmento(p,i);
-        return true;
-      }
-    }
-    System.out.println("No hay espacio suficiente para el proceso, se debe ejecutar o eliminar un proceso");
-    return false;
-  }
-
-  /*Insertar es un poco laborioso, al manejar como tal una lista, se debe asegurar
-  que los elementos sean coherentes en todo momento, y especialmente como estamos
-  tratando con una lista que funge como la abstraccion de memoria hay que tener cuidado
-  a la hora de manejar los registros base, limite y las separaciones*/
-  void insertarSegmento(Proceso aInsertar, int n){
-    /*Con este if, podemos verificar una cosa muy importante y el mejor de los casos, que las
-    localidades del proceso justo coincidan con el tamaño del segmento, en este caso, podemos
-    insertar el proceso sin mover ningun registro*/
-    if (memoriaPrincipal.get(n).limite - memoriaPrincipal.get(n).base == aInsertar.r_localidad){
-      memoriaPrincipal.get(n).P = aInsertar;
-    } else{
-      /*De otra manera, se divide la lista, esto porque tenemos un espacio vacio y otro que
-      no lo va a estar, entonces hay que modificar los registros base y limite, el registro
-      base es facil, simplemente es el que era antes de dividirse, con el registro limite
-      del nuevo segmento debemos sumar el base mas el tamaño de la localidad del proceso,
-      por fortuna este mismo valor cuenta como el nuevo valor base del segmento vacio, y el
-      registro limite seguira siendo el mismo que era originalmente */
-      Segmento S = new Segmento();
-      S.P = aInsertar;
-      S.base = memoriaPrincipal.get(n).base;
-      S.limite = S.base + aInsertar.r_localidad;
-      memoriaPrincipal.get(n).base = S.limite;
-      memoriaPrincipal.add(n,S);
-    }
-  }
-  /*Esta funcion recorre la lista de memoria e imprime los nombres
-  del proceso de cada segmento asi como su valor de base y limite*/
-  void imprimirListaMemoria(){
-    for( int i = 0 ; i<memoriaPrincipal.size(); i++){
-      System.out.println("Segmento numero: " + (i+1) );
-      System.out.println("Nombre del proceso: " + memoriaPrincipal.get(i).P.nombre);
-      System.out.println("Base: " +memoriaPrincipal.get(i).base);
-      System.out.println("Limite: " +memoriaPrincipal.get(i).limite);
-    }
-  }
-  /*Esta funcion solo se asegura de que haya espacio en memoria, no le interesa cuanto, esto
-  es solo para verificar si se podria crear un proceso o no, sin embargo tambien se toma en cuenta
-  el caso para cuando el espacio disponible es menor a 64 localidades, en ese caso directamente
-  dice que no hay memoria*/
-  boolean buscarEspacio(){
-    int minimo;
-    for( int i = 0; i<memoriaPrincipal.size(); i++){
-      minimo = memoriaPrincipal.get(i).limite - memoriaPrincipal.get(i).base;
-      if(memoriaPrincipal.get(i).P.nombre == "Vacio" && minimo>=64)
-        return true;
-      }
-  return false;
-  }
-
-  
-}
+//---------------------------------------------------------------------------------------------------------------
 
 
 
----------------------------------------------------------------------------------------------------------------
-  
-  
-  
   public class gestionMemoria {
     //ManejoProcesos controlador = new ManejoProcesos();
     /*En esta implementacion usa la idea de cola de memoria, por lo cual
@@ -114,7 +14,7 @@ public class gestionMemoria {
     de si este vacio o tenga algun segmento*/
 
     List<Segmento> memoriaPrincipal = new ArrayList<Segmento>();
-    
+
     //Aqui se puede observar el objeto segmento, el cual sera la unidad basica
     //de nuestra estructura memoriaPrincipal, la cual es una lista
     public class Segmento{
@@ -178,7 +78,7 @@ public class gestionMemoria {
             memoriaPrincipal.add(n,S);
         }
     }
-    
+
     /*Esta funcion recorre la lista de memoria e imprime los nombres
     del proceso de cada segmento asi como su valor de base y limite*/
     void imprimirListaMemoria(){
@@ -189,7 +89,7 @@ public class gestionMemoria {
             System.out.println("Limite: " +memoriaPrincipal.get(i).limite+"\n");
         }
     }
-    
+
     /*Esta funcion solo se asegura de que haya espacio en memoria, no le interesa cuanto, esto
     es solo para verificar si se podria crear un proceso o no, sin embargo tambien se toma en cuenta
     el caso para cuando el espacio disponible es menor a 64 localidades, en ese caso directamente
@@ -198,12 +98,12 @@ public class gestionMemoria {
         int minimo;
         for( int i = 0; i<memoriaPrincipal.size(); i++){
         minimo = memoriaPrincipal.get(i).limite - memoriaPrincipal.get(i).base;
-        if(memoriaPrincipal.get(i).P.nombre == "Vacio" && minimo>=64)
+        if(memoriaPrincipal.get(i).P.nombre == "Vacio" && minimo>=64 )
             return true;
         }
         return false;
     }
-    
+
     void estadoSistema(ManejoProcesos controlador){
         System.out.println("Número de procesos preparados para ejecutarse: "+controlador.Preparados.size()+"\n");
         System.out.println("Proceso finalizados exitosamente: ");
@@ -213,7 +113,7 @@ public class gestionMemoria {
         System.out.println("\nEstado de la memoria:");
         this.imprimirListaMemoria();
     }
-    
+
     boolean quitarProcesoDeMemoria(Proceso p, ManejoProcesos controlador){
         boolean liberada = false;
         int baseProcesoAEliminar, limiteProcesoAEliminar;
@@ -222,17 +122,17 @@ public class gestionMemoria {
             int anterior, siguiente;
             //Si se localiza el segmento de memoria del proceso a eliminar
             if(memoriaPrincipal.get(i).P.idP == p.idP){
-                
+
                 limiteProcesoAEliminar = memoriaPrincipal.get(i).limite;
                 baseProcesoAEliminar = memoriaPrincipal.get(i).base;
                 anterior = i-1;
                 siguiente = i+1;
-                //Se revisa si es el primer elemento de la lista, pues este no puede tener anterior 
+                //Se revisa si es el primer elemento de la lista, pues este no puede tener anterior
                 if(i == 0){
                     if(siguiente < memoriaPrincipal.size()){
                         //Tienen elementos siguientes
                         if (memoriaPrincipal.get(siguiente).P.nombre == "Vacio") {
-                            //Su elemento siguiente es vacío 
+                            //Su elemento siguiente es vacío
                             //Se juntará lo del segmento vacío y lo del segmento del proceso que se eliminará
                             //La base se modifica y el límite se queda igual
                             memoriaPrincipal.get(siguiente).base = baseProcesoAEliminar;
@@ -241,13 +141,13 @@ public class gestionMemoria {
                             break;
                         }else{ //no tiene vacio a los lados se cambia a vacio el nombre
                         //Si no hay ningún espacio vacío junto a este segmento, se cambia el nombre del proceso a vacío
-                        memoriaPrincipal.get(i).P.nombre = "Vacío";
+                        memoriaPrincipal.get(i).P.nombre = "Vacio";
                         liberada = true;
                                 break;
                         }
                     }
                 }else{
-                   //Si no es el primer elemento de la lista 
+                   //Si no es el primer elemento de la lista
                     if (memoriaPrincipal.get(anterior).P.nombre == "Vacio") {
                         //Se juntará lo del segmento vacío y lo del segmento del proceso que se eliminará
                         //La base se queda igual y el límite se modifica
@@ -255,7 +155,7 @@ public class gestionMemoria {
                         memoriaPrincipal.remove(i);
                         liberada = true;
                         break;
-                     //Si no fue la localidad anterior 
+                     //Si no fue la localidad anterior
                      //Si hay un segmentos después en la memoria
                     }else if(siguiente < memoriaPrincipal.size()){
                         if (memoriaPrincipal.get(siguiente).P.nombre == "Vacio") {
@@ -268,7 +168,7 @@ public class gestionMemoria {
                         }
                         else{//mismo problemaa
                             //Si no hay ningún espacio vacío junto a este segmento, se cambia el nombre del proceso a vacío
-                            memoriaPrincipal.get(i).P.nombre = "Vacío";
+                            memoriaPrincipal.get(i).P.nombre = "Vacio";
                             liberada = true;
                             break;
                         }
@@ -289,13 +189,13 @@ public class gestionMemoria {
                 procesoAEliminar = memoriaPrincipal.get(i).P;
                 //Voy a quitarlo de la memoria y devuelve si fue eliminado
                 eliminado = this.quitarProcesoDeMemoria(procesoAEliminar, controlador);
-                //Si fue eliminado 
+                //Si fue eliminado
                 if(eliminado == true){
                     if (codigo == 1) {
-                        //Agrego al proceso a la lista de Eliminados 
+                        //Agrego al proceso a la lista de Eliminados
                         controlador.Eliminados.add(procesoAEliminar);
                     }
-                    //Lo busco en la lista de preparados para eliminarlo 
+                    //Lo busco en la lista de preparados para eliminarlo
                     for (int j = 0; j < controlador.Preparados.size(); j++) {
                         //Si está en la lista de preparados lo elimino
                         if(controlador.Preparados.get(j).idP == procesoAEliminar.idP){
@@ -312,6 +212,3 @@ public class gestionMemoria {
         return eliminado;
     }
 }
-
-
-
