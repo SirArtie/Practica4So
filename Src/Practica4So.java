ec1c6c9cc5a761940a0bf3bb50524d99a81b601c
teredl8 <51178588+teredl8@users.noreply.github.com>
2021-11-06 02:58:09 -0600

    Update Practica4So.java

--- Src/Practica4So.java
+++ Src/Practica4So.java
@@ -15,23 +15,25 @@
      * @param args the command line arguments
      */
   public class Practica4So{
-
+   
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         // TODO code application logic here
-
         System.out.println("\n****************************************************************");
         System.out.println("********************** GESTIÓN DE MEMORIA **********************");
         System.out.println("****************************************************************\n");
-
-        String m = "MENÚ DE OPCIONES:\n1.-Crear Proceso Nuevo\n2.-Ver estado actual del Sistema\n3.-Imprimir Cola de Procesos\n4.-Ver proceso actual\n5.-Ejecutar proceso actual\n6.-Pasar al proceso siguiente\n7.-Matar proceso actual\n8.-Salir del programa\n";
+        
+        String m = "MENÚ DE OPCIONES:\n1.-Crear Proceso Nuevo\n2.-Ver estado actual de los procesos\n3.-Imprimir Cola de Procesos\n4.-Ver proceso actual\n5.-Ejecutar proceso actual\n6.-Pasar al proceso siguiente\n7.-Matar proceso actual\n8.- Ver estado de la memoria\n9.-Desfragmentar memoria\n10.-Salir del programa\n";
         int opcion;
-
+        
         //Se inicializa el administrador del manejo de procesos
         ManejoProcesos controlador = new ManejoProcesos();
-
+        
         //Se inicializa el administrador de la memoria
         gestionMemoria memoria = new gestionMemoria();
-
+        
         memoria.memoriaVacia();
         do{
             //Funcion que permite pedir un meno continuamente
@@ -48,8 +50,8 @@
                     break;
                 case 2:
                     System.out.println("\n----------------------------------------------------------------\n");
-                    System.out.println("******************** Estado actual del sistema ********************\n");
-                    memoria.estadoSistema(controlador);
+                    System.out.println("******************** Estado de los procesos ********************\n");
+                    memoria.estadoProcesos(controlador);
                     System.out.println("\n----------------------------------------------------------------\n");
                     break;
                 case 3:
@@ -74,7 +76,7 @@
                 case 6:
                     System.out.println("\n----------------------------------------------------------------\n");
                     System.out.println("******************** Pasar al proceso siguiente ********************\n");
-                    controlador.PasarProcesoSiguiente();
+                    controlador.PasarProcesoSiguiente();                    
                     System.out.println("\n----------------------------------------------------------------\n");
                     break;
                 case 7:
@@ -90,14 +92,30 @@
                         System.out.println("El proceso seleccionado se ha eliminado");
                     }
                     else{
-                        System.out.println("\"ERROR: Ingrese datos correctos\"");
+                        System.out.println("\nERROR: Ingrese datos correctos\n");
                     }
                     System.out.println("\n----------------------------------------------------------------\n");
                     break;
                 case 8:
                     System.out.println("\n----------------------------------------------------------------\n");
+                    System.out.println("******************** Estado de la memoria ********************\n");
+                    memoria.estadoMemoria();
+                    System.out.println("\n----------------------------------------------------------------\n");
+                    break;
+                case 9:
+                    System.out.println("\n----------------------------------------------------------------\n");
+                    System.out.println("******************** Desfragmentar memoria ********************\n");
+                    memoria.desfragmentarMemoria();
+                    System.out.println("Estado de la memoria:");
+                    memoria.estadoMemoria();
+                    System.out.println("\n----------------------------------------------------------------\n");
+                    break; 
+                case 10:
+                    System.out.println("\n----------------------------------------------------------------\n");
                     System.out.println("El estado final del sistema es: \n");
-                    memoria.estadoSistema(controlador);
+                    memoria.estadoProcesos(controlador);
+                    System.out.println("\nEstado memoria:");
+                    memoria.estadoMemoria();
                     System.out.println("\nAdios, vuelva pronto");
                     System.out.println("\n----------------------------------------------------------------\n");
                     break;
@@ -105,10 +123,7 @@
                     System.out.println("La opción que ha insertado no es valida");
                     break;
             }
-            //memoria.buscarSegmentoVacio();
-            //Uso esta funcion para ver el estado de la memoria
-            //memoria.imprimirListaMemoria();
-        }while(opcion!=8);
+        }while(opcion!=10);
     }
-
+    
 }
